7e0c387b4f0cb07739b1ad826d35ff6b3b4a4c39
Gonzalo Fernández <gonzalofn95@gmail.com>
2026-01-26 09:05:28 +0100

    Metroid

---
+++ 3 Farmer/scenes/player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get('parameters/MoveStateMachine/playback')
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var direction: Vector2
+
+
+func get_input():
+	direction = Input.get_vector("left", "right", "up", "down")
+	print(direction)
+	
+func _physics_process(_delta: float) -> void:
+	get_input()
+	velocity = direction * SPEED
+	move_and_slide()
+	animation()
+
+func animation():
+	if direction:
+		move_state_machine.travel('move')
+		var target_vector = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set('parameters/MoveStateMachine/move/blend_position',target_vector)
+		$AnimationTree.set('parameters/MoveStateMachine/idle/blend_position',target_vector)
+	else:
+		move_state_machine.travel('idle')
