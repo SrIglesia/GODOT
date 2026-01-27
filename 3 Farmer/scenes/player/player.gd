3c0bd3debfcd29d30181dd779e37ccbffd3ea1c0
Gonzalo Fernández <gonzalofn95@gmail.com>
2026-01-27 09:23:18 +0100

    Progress in game 3

--- 3 Farmer/scenes/player/player.gd
+++ 3 Farmer/scenes/player/player.gd
@@ -1,19 +1,38 @@
 extends CharacterBody2D
 
 @onready var move_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get('parameters/MoveStateMachine/playback')
+@onready var tool_state_machine: AnimationNodeStateMachinePlayback = $AnimationTree.get('parameters/ToolStateMachine/playback')
+
+enum Tools {HOE, AXE, WATER}
+var current_tool: Tools = Tools.AXE
+const tool_connection = {
+	Tools.HOE: 'hoe',
+	Tools.AXE: 'axe',
+	Tools.WATER: 'water',
+}
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var direction: Vector2
-
+var can_move = true
 
 func get_input():
 	direction = Input.get_vector("left", "right", "up", "down")
-	print(direction)
 	
+	if Input.is_action_just_pressed('action'):
+		tool_state_machine.travel(tool_connection[current_tool])
+		$AnimationTree.set('parameters/OneShot/request', AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		can_move = false
+		
+	if Input.is_action_just_pressed('tool_forward') or Input.is_action_just_pressed('tool_backward'):
+		var tool_direction = Input.get_axis('tool_backward', 'tool_forward') as int
+		current_tool = posmod(current_tool + tool_direction, Tools.size()) as Tools
+		print(current_tool)
+
 func _physics_process(_delta: float) -> void:
-	get_input()
-	velocity = direction * SPEED
+	if can_move == true:
+		get_input()
+	velocity = direction * SPEED * int(can_move)
 	move_and_slide()
 	animation()
 
@@ -23,5 +42,12 @@ func animation():
 		var target_vector = Vector2(round(direction.x), round(direction.y))
 		$AnimationTree.set('parameters/MoveStateMachine/move/blend_position',target_vector)
 		$AnimationTree.set('parameters/MoveStateMachine/idle/blend_position',target_vector)
+		for state in tool_connection.values():
+			$AnimationTree.set('parameters/ToolStateMachine/' + state + '/blend_position',target_vector)
 	else:
 		move_state_machine.travel('idle')
+
+
+func _on_animation_tree_animation_finished(_anim_name: StringName) -> void:
+	can_move = true
+	
